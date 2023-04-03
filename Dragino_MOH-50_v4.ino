d2b58dee224dc34c30d46643830c54cbe16a4c40
antontoniavskyi <53255050+antontoniavskyi@users.noreply.github.com>
2023-04-03 19:37:44 +0300

    Added server, have started work on transferin mine info

--- Dragino_MOH-50_v4.ino
+++ Dragino_MOH-50_v4.ino
@@ -4,11 +4,14 @@
 #include <BridgeClient.h>
 #include <Ethernet.h>
 
+byte mac[] = { 0xA8, 0x40, 0x41, 0x1B, 0xF1, 0x10 };
+EthernetServer server(80);
+
 #include <ArduinoJson.h>
 StaticJsonDocument<255> docInput;
 StaticJsonDocument<255> docOutput;
+StaticJsonDocument<255> docMineArray;
 String jsonOutput;
-
 
 String response;
 char buff[300];
@@ -28,6 +31,15 @@ BridgeClient client;
 
 bool relay = true;
 
+struct mineStruct{
+  String structStatus;
+  double structLatitude;
+  double structLongitude;
+};
+
+const byte sizeOfArray = 10;
+int structArrayOfMine[sizeOfArray];
+
 void setup() {
   Bridge.begin(115200);
   Console.begin();
@@ -35,6 +47,12 @@ void setup() {
   while (!Serial) {}
 
   Console.println("LoRa Receiver");
+
+  Ethernet.begin(mac);
+  server.begin();
+
+  mineStruct mineqwerty = {"OK", 50.443222, 30.447847}; //3792142 is mine ID
+  structArrayOfMine[0] = &mineqwerty;
 
   if (!LoRa.begin(freq)) {
     Console.println("Starting LoRa failed!");
@@ -62,14 +80,14 @@ void onReceive() {
   bool receivedPackageIndex;
 
   while (LoRa.available()) {
-    incomingstring = LoRa.readString();
+    incomingString = LoRa.readString();
   }
 
-  deserializeJson(docInput, incomingstring);
+  deserializeJson(docInput, incomingString);
 
   int packetNumb = docInput["packetNumber"];
-  double latitude = doc["gps"][0];
-  double longitude = doc["gps"][1];
+  double latitude = docInput["gps"][0];
+  double longitude = docInput["gps"][1];
   bool relay = docInput["relay"];
 
   /*Рахуємо унікальні пакети. У разі, якщо отримуємо кілька пакетів з однаковою інформацією,
@@ -82,11 +100,11 @@ void onReceive() {
     latitudeBuff = latitude;
     longitudeBuff = longitude;
     relayBuff = relay;
-    recivedPacketInfo = "Received " + (String)receivePckCounter + " packets with RSSI " + (String)LoRa.packetRssi() + " and SNR " + (String)LoRa.packetSnr();
+    recivedPacketInfo = "Received " + (String)receivedPackageCounter + " packets with RSSI " + (String)LoRa.packetRssi() + " and SNR " + (String)LoRa.packetSnr();
   } else if (packetNumb > pckbuff) {
     pckbuff = packetNumb;
-    receivePckCounter = 0;
-    if (receivedpackageindex) {
+    receivedPackageCounter = 0;
+    if (receivedPackageIndex) {
       Console.println(recivedPacketInfo);
       Console.print("Latitude: ");
       Console.println(latitudeBuff);
@@ -160,4 +178,53 @@ void sendPacket(String packetOutput) {
   LoRa.beginPacket();
   LoRa.print(packetOutput);
   LoRa.endPacket();
+}
+
+void serializeJsonWithMineInfo() {
+  JsonObject mine1 = docMineArray.createNestedObject();
+  mine1["ID"] = structArrayOfMine[0];
+  mine1["status"] = structArrayOfMine[0].structStatus;
+  mine1["gps"][0] = structArrayOfMine[0].structLatitude;
+  mine1["gps"][2] = structArrayOfMine[0].structLongitude;
+}
+
+void mineServer() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Console.println("new client");
+    // an HTTP request ends with a blank line
+    bool currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Console.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the HTTP request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard HTTP response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");         // refresh the page automatically every 5 sec
+          serializeJsonPretty(docMineArray, client);
+          client.println();
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Console.println("client disconnected");
+  }
 }
