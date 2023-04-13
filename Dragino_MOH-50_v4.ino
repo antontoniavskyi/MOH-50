2d63088fcca42b048d3ccf0b0bf15dfc4ff5697a
antontoniavskyi <53255050+antontoniavskyi@users.noreply.github.com>
2023-04-13 22:29:40 +0300

    Added FileIO

--- Dragino_MOH-50_v4.ino
+++ Dragino_MOH-50_v4.ino
@@ -2,10 +2,7 @@
 #include <LoRa.h>
 #include <Console.h>
 #include <BridgeClient.h>
-#include <Ethernet.h>
-
-byte mac[] = { 0xA8, 0x40, 0x41, 0x1B, 0xF1, 0x10 };
-EthernetServer server(80);
+#include <FileIO.h>
 
 #include <ArduinoJson.h>
 StaticJsonDocument<100> docInput;
@@ -37,13 +34,10 @@ struct mineStruct mineqwerty[10];
 void setup() {
   Bridge.begin(115200);
   Console.begin();
-  Serial.begin(115200);
-  while (!Serial) {}
+  FileSystem.begin();
 
+  while(!Console);
   Console.println("LoRa Receiver");
-
-  Ethernet.begin(mac);
-  server.begin();
 
   //mineqwerty[0] = { strcpy(mineqwerty[0].structId,"an34ju141ol"), "OK", 50.443222, 30.447847 };  //3792142 is mine ID
   strcpy(mineqwerty[0].structId, "an34ju141ol");
@@ -62,16 +56,16 @@ void setup() {
 }
 
 void loop() {
-  Console.print("Server is at ");
-  Console.println(Ethernet.localIP());
-  mineServer();
   if (LoRa.parsePacket()) {
     Serial.println("receive");
     onReceive();
   } else {
     readConsole();
+    Console.print("2");
+    serializeJsonWithMineInfo();
   }
   LoRa.receive();
+  Console.print("1");
 }
 
 void onReceive() {
@@ -96,9 +90,9 @@ void onReceive() {
   bool relay = docInput["relay"];
 
   /*
-   Рахуємо унікальні пакети. У разі, якщо отримуємо кілька пакетів з однаковою інформацією,
-   ми їх зберігаємо у буфері, а виводимо коли отримуємо новий пакет даних, 
-   таким чином кількість отриманих унікальних пакетів буде актуальною
+    Рахуємо унікальні пакети. У разі, якщо отримуємо кілька пакетів з однаковою інформацією,
+    ми їх зберігаємо у буфері, а виводимо коли отримуємо новий пакет даних,
+    таким чином кількість отриманих унікальних пакетів буде актуальною
   */
   if (packetNumb == packageBuffNumb) {
     receivedPackageIndex = true;
@@ -187,50 +181,26 @@ void sendPacket(String packetOutput) {
 }
 
 void serializeJsonWithMineInfo() {
+  String mineInfoOutput;
   JsonObject mine1 = docMineArray.createNestedObject();
   mine1["ID"] = mineqwerty[0].structId;
   mine1["status"] = mineqwerty[0].structStatus;
   mine1["gps"][0] = mineqwerty[0].structLatitude;
-  mine1["gps"][2] = mineqwerty[0].structLongitude;
+  mine1["gps"][1] = mineqwerty[0].structLongitude;
+  //JsonArray mine1_gps = mine1.createNestedArray("gps");
+  //mine1_gps.add(mineqwerty[0].structLatitude);
+  //mine1_gps.add(mineqwerty[0].structLongitude);
+  serializeJson(docMineArray, mineInfoOutput);
+  writeMineInfoToFile(mineInfoOutput);
 }
 
-void mineServer() {
-  // listen for incoming clients
-  EthernetClient client = server.available();
-  if (client) {
-    Console.println("new client");
-    // an HTTP request ends with a blank line
-    bool currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Console.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the HTTP request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-          // send a standard HTTP response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");         // refresh the page automatically every 5 sec
-          //serializeJsonPretty(docMineArray, client);
-          client.println("FUCK");
-          break;
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    // give the web browser time to receive the data
-    delay(1);
-    // close the connection:
-    client.stop();
-    Console.println("client disconnected");
+void writeMineInfoToFile(String mineInfo) {
+  File dataFile = FileSystem.open("/www/mineServer/mineInfo", FILE_APPEND);
+  if (dataFile) {
+    dataFile.println(mineInfo);
+    dataFile.close();
+    Console.println(mineInfo);
+  }else {
+    Console.println("error opening datalog.csv");
   }
 }
