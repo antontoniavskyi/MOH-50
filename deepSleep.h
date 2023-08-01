4b14d1c6bb46a7509ea15715e59b8cfea9d0a4a7
antontoniavskyi <53255050+antontoniavskyi@users.noreply.github.com>
2023-08-01 13:35:58 +0300

    Update deepSleep.h
    Deleted "#include <SX126x-Arduino.h>"

--- deepSleep.h
+++ deepSleep.h
@@ -1,6 +1,5 @@
 #include "rom/rtc.h"
 #include <driver/rtc_io.h>
-#include <SX126x-Arduino.h>
 
 #define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
 #define TIME_TO_SLEEP  2        /* Time ESP32 will go to sleep (in seconds) */
